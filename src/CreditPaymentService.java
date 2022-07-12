0f5e47acb7989c9fe5011dc0b9a7bc7853735f0b
andreygork <agorkaev99@gmail.com>
2022-07-12 16:37:21 +0300

    CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
-    public double calculate(double amount, double percent , double term) {
-        double termMonth = term * 12 ;
+    public double calculate(double amount, double percent, double term) {
+        double termMonth = term * 12;
         double percentMonth = percent / 1200;
         double month = Math.pow(1 + percentMonth, termMonth);
         double payment = amount * ((percentMonth * month) / (month - 1));
